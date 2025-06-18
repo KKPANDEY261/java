b87d430af763042ead171930c166c4ce94b2a6aa
KKPANDEY261 <karanpandey261@gmail.com>
2025-06-18 15:28:33 +0530

    submit

---
+++ declarevariable.java
@@ -0,0 +1,32 @@
+public class declarevariable {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b=15;
+        System.out.println(a+b);
+
+        String c="Welcome ";
+        String d="to nsti";
+        System.out.println(c+d);
+
+    }
+    public static int add(){
+
+        int a=10;
+        int b=30;
+        return a+b;
+    }
+    public static void sum(){
+
+        int a=10;
+        int b=30;
+        
+        System.out.println(a+b);
+    }
+
+
+
+
+
+    
+}
