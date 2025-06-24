fd54e51eb28331adf21b3797415068a504f6a594
KKPANDEY261 <karanpandey261@gmail.com>
2025-06-24 10:22:30 +0530

    Arithmatic operator is completed

--- operator.java
+++ operator.java
@@ -1,5 +1,19 @@
 public class operator {
     public static void main(String[] args) {
-        
+    //Arithmatic operator in java
+        int a,b,c;
+        a=10;
+        b=20;
+        c=25;
+    System.out.println(a+b);       //Output:- 30
+    System.out.println(b-a);       //Output:- 10
+    System.out.println(a*b);       //Output:- 200
+    System.out.println(b/a);       //Output:- 2
+    System.out.println(c%a);       //Output:- 5
+    System.out.println(a++);       //Output:- 10
+    System.out.println(a--);       //Output:- 11
+    System.out.println(++a);       //Output:- 11
+    System.out.println(--a);       //Output:- 10
     }
+    
 }
