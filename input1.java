f7635e1116d3dd330078e28f1805dc4686d3de5c
KKPANDEY261 <karanpandey261@gmail.com>
2025-06-26 11:57:43 +0530

    input final

--- input1.java
+++ input1.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class input1 {
+
+      public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Provide your name:-");
+        String name=scanner.nextLine();
+        System.out.println("Provide your name:-");
+        long name1=scanner.nextLong();
+        System.out.println("hello my first name is "+name+" and your mobile number is "+name1);
+        scanner.close();
+        
+      }
       
 }
