1627bd41e9c9c27af27b84789dab455f85460fa8
KKPANDEY261 <karanpandey261@gmail.com>
2025-06-26 11:15:05 +0530

    final operator

--- operator.java
+++ operator.java
@@ -14,6 +14,7 @@ public class operator {
     System.out.println("Post Decrement:-"+(a--));       //Output:- 11
     System.out.println("Pre Increment:-"+(++a));       //Output:- 11
     System.out.println("Pre Decrement:-"+(--a));       //Output:- 10
+    System.out.println(Math.pow(2, 30)); //output 25   exponentation method
 
 
     //Assignment operator 
@@ -53,7 +54,37 @@ public class operator {
          System.out.println(k<=j);     // output    true
          System.out.println(j<=l);     // output    true
 
+    //logical operator
+       System.out.println("logical AND:-"+(55==55 && 66>56));     //output    logical AND:-true
+        System.out.println("logical AND:-"+(55==55 && 66<56));   //output    logical AND:-false
+        System.out.println("logical OR:-"+(55==55 || 66<56));   //output    logical OR:-true
+         System.out.println("logical OR:-"+(55==55 || 66>56)); //output    logical OR:-true
+          System.out.println("logical NOT:-"+(!(55==55)));    //output    logical NOT:-false
 
+
+    //Bitwise operator
+       System.out.println("Bitwise AND:-"+(5&6));     //output  Bitwise AND:-4
+       System.out.println("Bitwise OR:-"+(5|6));     //output  Bitwise OR:-7
+       System.out.println("Bitwise XOR:-"+(5^6));   //output  Bitwise XOR:-3
+       System.out.println("Bitwise NOT:-"+(~5));   //output  Bitwise NOT:--6
+       System.out.println("Bitwise Left Shift:-"+(5<<2));  //output Bitwise Left Shift:-20
+       System.out.println("Bitwise Right Shift:-"+(15>>2));  //output Bitwise Right Shift:-3
+       System.out.println("Bitwise Unsigned Right Shift:-"+(-5>>>1)); //output Bitwise Unsigned Right Shift:-2147483645
+
+    //Ternary Operator
+      String m=(5>3)? "hello": "Welcome";
+      String n=(5<3)? "hello": "Welcome";
+       System.out.println("Ternary:-"+ m);    // output Ternary:-hello
+       System.out.println("Ternary:-"+ n);   // output Ternary:-Welcome
+
+    //String operator 
+        System.out.println("Hello "+"my "+"name "+"is "+"Pandey" );  //output Hello my name is Pandey
+    
+
+    //instanceof Operator
+           String s = "Hello";
+             boolean result = s instanceof String; 
+             System.out.println(result);     // output true
 
     
     }
