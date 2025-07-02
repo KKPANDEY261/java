8cc25a46cefd8223a23b2b7e2e448ae4212816f9
KKPANDEY261 <karanpandey261@gmail.com>
2025-07-02 09:55:01 +0530

    control flow statement

---
+++ controlflow.java
@@ -0,0 +1,48 @@
+import java.lang.classfile.instruction.StoreInstruction;
+
+public class controlflow {
+    public static void main(String[] args) {
+    int Score=88;
+    String Grade;
+    String Status;
+    
+    if (Score >= 90){
+        Grade="A";
+    }
+    else if (Score >= 70){
+        Grade="B";
+    }
+    else if (Score >= 50){
+        Grade="C";
+    }
+    else if (Score >=30){
+        Grade="D";
+    }
+    else {
+        Grade="E";
+    }
+
+    switch (Grade){
+        case "A":
+                 Status="Excelent";
+                 break;
+        case "B":
+                 Status="Very Good";
+                 break;
+        case "C":
+                 Status="Good";
+                 break;
+        case "D":
+                 Status="Poor performance";
+                 break;
+        default :
+                 Status="Failed";
+                 break;
+    }
+
+    System.out.println(Score);
+    System.out.println(Grade);
+    System.out.println(Status);
+
+    }
+}
