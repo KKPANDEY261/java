e46fefbf10c96bfd70d6a8c2a5ee1e3f3a390cef
KKPANDEY261 <karanpandey261@gmail.com>
2025-07-03 14:58:06 +0530

    loops

---
+++ whileloop.java
@@ -0,0 +1,9 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int a=1;
+        while (a<=20){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
